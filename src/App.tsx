f12723d50d4672e9ef92a07a78ac37a4d9f4e949
Sam M <sam.werter345@gmail.com>
2023-09-12 18:58:16 -0400

    website passes checks

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,43 @@
 import React from "react";
 import "./App.css";
+import img from "./cat-551554_640.jpg";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UM COS420 with React Hooks and TypeScript
+            <header>
+                <h1 className="App-header">COS420 Website by Sam Minor</h1>
             </header>
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Sam Minor... Hello World!
+                Sam Minor says Hello World!
+                <Container>
+                    <Row>
+                        <Col>
+                            <div className="App">
+                                <div className="App-rectangle"></div>
+                            </div>
+                        </Col>
+                    </Row>
+                    <p> </p>
+                    <Row>
+                        <Col>
+                            <div className="App">
+                                <div className="App-rectangle"></div>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+                <Button onClick={() => console.log("Hello World!")}>
+                    Log Hello World
+                </Button>
+                <img src={img} alt="a picture of a kitty cat"></img>
+                <p>some attributes of a cat</p>
+                <ul>
+                    <li> ears </li>
+                    <li> whiskers </li>
+                    <li> fur </li>
+                </ul>
             </p>
         </div>
     );
