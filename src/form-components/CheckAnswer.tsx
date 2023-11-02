dd8d76211db06500e7bf1719d7c23cbfcb0974bb
Samuel Minor <79673482+SamMi777@users.noreply.github.com>
2023-11-02 17:10:58 -0400

    fixed CheckAnswer component

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [exAnswer, setExAnswer] = useState<string>("");
+
+    function newAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setExAnswer(event.target.value);
+    }
     return (
         <div>
             <h3>Check Answer</h3>
+            <input
+                value={exAnswer}
+                onChange={newAnswer}
+                placeholder="Enter Answer"
+            />
+            {exAnswer === expectedAnswer ? <div>✔️</div> : <div>❌</div>}
         </div>
     );
 }
